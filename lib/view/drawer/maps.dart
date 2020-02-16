7870aa96e1aad514520b9fa05a641b10be0cad84
Yoann Android Studio <yoann.chappaz@outlook.com>
2020-02-16 13:54:13 +0100

    Adding maps module on app. We can show all partners on the maps and there is a button to redirect to maps. If possible, I will trying to add user position.

--- lib/view/drawer/maps.dart
+++ lib/view/drawer/maps.dart
@@ -1,5 +1,6 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
-import 'package:ba_locale/model/design.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class Maps extends StatefulWidget {
   @override
@@ -7,6 +8,29 @@ class Maps extends StatefulWidget {
 }
 
 class MapsState extends State<Maps> {
+  GoogleMapController mapController;
+  final LatLng _initial = const LatLng(47.471780, -0.551767);
+  final Map<String, Marker> _markers = {};
+
+  Future<void> _onMapCreated(GoogleMapController controller) async {
+    final QuerySnapshot partners = await Firestore.instance
+        .collection("companies")
+        .getDocuments();
+    setState(() {
+      _markers.clear();
+      for (DocumentSnapshot partner in partners.documents) {
+        final marker = Marker(
+          markerId: MarkerId(partner.documentID),
+          position: LatLng(partner.data['latitude'], partner.data['longitude']),
+          infoWindow: InfoWindow(
+            title: partner.data['name'],
+            snippet: partner.data['address'],
+          ),
+        );
+        _markers[partner.data['name']] = marker;
+      }
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -16,14 +40,13 @@ class MapsState extends State<Maps> {
           centerTitle: true
       ),
       body: Builder(builder: (BuildContext context) {
-        return Form(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: <Widget>[
-              InputDesign("Maps"),
-            ],
+        return GoogleMap(
+          onMapCreated: _onMapCreated,
+          initialCameraPosition: CameraPosition(
+            target: _initial,
+            zoom: 15.0,
           ),
+          markers: _markers.values.toSet(),
         );
       }),
     );
